2241d9774f35b29e7ee4f2fbed2b22134f580524
jac <jacdtaylor@gmail.com>
2024-10-27 17:54:07 -0600

    executeUpdate transfered over

--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -1,8 +1,13 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.UserData;
 
 import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 
 public class SqlUserDAO implements UserDAO{
 
@@ -19,6 +24,7 @@ public class SqlUserDAO implements UserDAO{
             CREATE TABLE IF NOT EXISTS  user (
               `username` varchar(256) NOT NULL,
               `password` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
               PRIMARY KEY (`username`),
               INDEX(username)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
@@ -40,14 +46,47 @@ public class SqlUserDAO implements UserDAO{
     }
 
 
-    @Override
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case UserData p -> ps.setString(i + 1, p.toString());
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {
+                        }
+                    }
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+         catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException("Database Error");
+        }}
+
+
+        @Override
     public UserData getUser(String username) throws DataAccessException {
         return null;
     }
 
     @Override
-    public void createUser(UserData user) {
-
+    public void createUser(UserData user) throws DataAccessException {
+        var statement = "INSERT INTO user (username, password, json) VALUES (?, ?, json)";
+        var json = new Gson().toJson(user);
+        var id = executeUpdate(statement, user.username(), user.password(), json);
     }
 
     @Override
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -9,7 +9,7 @@ public interface UserDAO {
 
     UserData getUser(String username) throws DataAccessException;
 
-    void createUser(UserData user);
+    void createUser(UserData user) throws DataAccessException;
     boolean confirmUsername(String username);
     void clear();
     boolean validateCredentials(UserData user) throws DataAccessException, InvalidCredentialException;
