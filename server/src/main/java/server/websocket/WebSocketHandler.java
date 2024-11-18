b97b49262887a8ffec0d5a3186bd0d231426df7c
jac <jacdtaylor@gmail.com>
2024-11-18 15:18:54 -0700

    Started WebSocketHandler

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,34 @@
 package server.websocket;
 
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
+
+@WebSocket
 public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connectUser(command.getAuthToken(), command.getGameID(), session);
+            case MAKE_MOVE -> makeMove(command.getAuthToken(), command.getGameID());
+            case LEAVE -> leaveUser(command.getAuthToken(), command.getGameID());
+            case RESIGN -> resignUser(command.getAuthToken(), command.getGameID());
+
+        }
+    }
+
+
+
+
+
 }
