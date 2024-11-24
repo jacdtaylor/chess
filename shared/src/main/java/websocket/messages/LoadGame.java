67883329aef26377ebbe16e21c9d34cb387aa6eb
jac <jacdtaylor@gmail.com>
2024-11-24 12:07:01 -0700

    Got rid of some unneeded stuff

--- shared/src/main/java/websocket/messages/LoadGame.java
+++ shared/src/main/java/websocket/messages/LoadGame.java
@@ -25,17 +25,5 @@ public class LoadGame extends ServerMessage{
         this.color = color;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        LoadGame loadGame = (LoadGame) o;
-        return Objects.equals(game, loadGame.game);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), game);
-    }
 }
--- shared/src/main/java/websocket/messages/Notification.java
+++ shared/src/main/java/websocket/messages/Notification.java
@@ -6,19 +6,7 @@ public class Notification extends ServerMessage{
 
     private final String message;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Notification that = (Notification) o;
-        return Objects.equals(message, that.message);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), message);
-    }
 
     public Notification(String message) {
         super(ServerMessageType.NOTIFICATION);
